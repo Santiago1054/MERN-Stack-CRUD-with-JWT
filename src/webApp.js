1424cb631e6889b393b8a48e78f62cb86f59ed6f
Santiago Ramirez Castellanos <ramirezcastellanossantiago@gmail.com>
2025-02-09 10:52:13 -0500

    Update webApp.js

--- src/webApp.js
+++ src/webApp.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Esto permite el envío de cookies\
-    origin: "http://localhost:5173",
+    origin: "https://mern-stack-crud-with-jwt-1.onrender.com",
   })
 );
 app.use(express.json());
